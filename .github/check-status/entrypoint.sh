1d17b0a128dcaf250aace2d7fc8917ab74736ba9
Alessandro Comodi <acomodi@antmicro.com>
2020-11-03 11:32:35 +0100

    test generation of latest link
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    
    test
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    
    test
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    
    test
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    
    test
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    
    test
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/check-status/entrypoint.sh
+++ .github/check-status/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 
-# Checking status of all CIs
-/check-status.py || echo '::set-output name=skip::true' && exit
-
-# If all CIs are successful, generate the latest artifact link
-mkdir install && symbiflow_get_latest_artifact_url > install/latest || echo '::set-output name=skip::true'
+/check-status.py && mkdir install && symbiflow_get_latest_artifact_url > install/latest || echo ::set-output name=skip::true
